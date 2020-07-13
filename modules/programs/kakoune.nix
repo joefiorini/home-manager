c0aedd250dd0f7d776cc087272bde72068c993b4
Joe Fiorini <joe@joefiorini.com>
2020-07-13 17:12:19 -0400

    Make kakoune package configurable

--- modules/programs/kakoune.nix
+++ modules/programs/kakoune.nix
@@ -629,6 +629,14 @@ in {
         default = { };
         description = "kakoune configuration options.";
       };
+      
+      package = mkOption {
+        type = types.package;
+        default = pkgs.kakoune;
+        description = ''
+          The package to use when installing kakoune
+         '';
+      };
 
       extraConfig = mkOption {
         type = types.lines;
@@ -653,7 +661,7 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home.packages = [ kakouneWithPlugins ];
+    home.packages = [ cfg.package ];
     xdg.configFile."kak/kakrc".source = configFile;
   };
 }
